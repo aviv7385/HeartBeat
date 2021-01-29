13b9a394ca18e57582a22f5de5ee4ce2bf5a0a2f
AvivE <aviv7385@gmail.com>
2021-01-29 20:19:59 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function heartBeat() {
+    //get DOM object
+    const heart = document.getElementById("heartDiv");
+    //assign animation 
+    heart.style.animation = "pulse 1s infinite linear";
+}
